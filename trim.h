f7309b7dbc261afa37f011912896137da664e0f4
ManuelKommenda <31957037+ManuelKommenda@users.noreply.github.com>
2017-12-06 16:33:37 +0100

    Trim

--- trim.h
+++ trim.h
@@ -10,3 +10,14 @@
  * Interface for trim
  * ----------------------------------------------------------
  */
+
+#ifndef __TRIM_H__
+#define __TRIM_H__
+
+#include <string.h>
+#include <stdlib.h>
+#define STRLEN 16
+
+void trim 	( 	const char * source,char * trimmed_string);
+
+#endif
